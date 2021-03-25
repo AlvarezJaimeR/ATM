166b0beb8c5db353170acf35cf4ffe63d3251206
AlvarezJaimeR <jrralvarez@gmail.com>
2021-03-24 22:13:13 -0400

    skeleton for the main function in index. added user prompt for main menu on the ATM

--- index.js
+++ index.js
@@ -7,11 +7,34 @@ const prompt = require('prompt-sync')({sigint: true});
 let currentBalance = valueBalance;
 console.log(currentBalance);
 
-const userInputPin = prompt('What is your 4 digit pin? ');
-let intValid = validateInput(userInputPin);
-console.log(intValid);
-console.log('Correct amount of digits and all numbers');
-let trialInputs = pin(userInputPin, intValid);
-console.log('Correct Pin');
 
-console.log('You entered ' + trialInputs);
+mainMenu();
+function mainMenu(){
+    const userInputPin = prompt('What is your 4 digit pin? ');
+    let intValid = validateInput(userInputPin);
+    console.log(intValid);
+    console.log('Correct amount of digits and all numbers');
+    let trialInputs = pin(userInputPin, intValid);
+    console.log('Correct Pin');
+    console.log('You entered ' + trialInputs);
+
+    let mainLoop = true;
+    while (mainLoop === true){
+    console.log('What would you like to do?' + '\n' + 'Balance' + '\n' + 'Withdraw' + '\n' + 'Deposit' + '\n' + 'Exit');
+    let userButton = prompt().toLowerCase();
+    console.log(userButton);
+
+        switch(userButton){
+            case 'balance':
+                console.log(currentBalance);
+                break;
+            case 'withdraw':
+                break;
+            case 'deposit':
+                break;
+            case 'exit':
+                mainLoop = false;
+                return mainLoop;
+        }
+    }
+}
